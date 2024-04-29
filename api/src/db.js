b06ccb526cddaa2a7f856d75f197f1605b23ba7f
seonyeonghun2 <kjca1117>
2024-04-29 14:47:31 +0900

    graphQL 샘플 데이터 정의

--- api/src/db.js
+++ api/src/db.js
@@ -1,43 +1,49 @@
 // mockup data
-const todos = [
+const notes = [
   {
     id: '1',
-    title: 'The moon',
-    content: 'Hello World!',
+    title: '자바스크립트 온라인 스터디(DeepDive)',
+    content: '이번주 발표주제 : GraphQL',
+    due_date: '2024-04-28T20:00:00.000Z',
     isDone: false,
+    invitee_id: '1',
   },
   {
     id: '2',
-    title: 'The Sunset',
-    content: 'say Goodbye',
+    title: '온라인 세미나 참석 - AI for Humans',
+    content: '온오프믹스 온라인 세미나(10:00)',
+    due_date: '2024-04-29T10:00:00.000Z',
     isDone: false,
+    invitee_id: '1',
   },
   {
     id: '3',
-    title: 'Time goes by',
-    content: 'Missing You!',
+    title: '업무 컨퍼런스',
+    content: '사업확장에 따른 업무 포지션 변경',
+    due_date: '2024-05-02T20:00:00.000Z',
     isDone: false,
+    invitee_id: '1',
   },
 ];
 
-const authors = [
+const invitees = [
   {
     id: '1',
-    fname: 'jack',
-    lname: 'daniel',
+    nickname: 'jack',
+    relation: 'friend',
     email: 'jackdaniel@me.com',
   },
   {
     id: '2',
-    fname: 'minhee',
-    lname: 'park',
-    email: 'smart_minhee@me.com',
+    nickname: 'minhee',
+    relation: 'co-worker',
+    email: 'smart_minhee@and.com',
   },
   {
     id: '2',
-    fname: 'heejin',
-    lname: 'min',
-    email: 'newjeans_mom@me.com',
+    nickname: 'heejin',
+    relation: 'friend',
+    email: 'newjeans_mom@you.com',
   },
 ];
-export default { todos, authors };
+export default { notes, invitees };
